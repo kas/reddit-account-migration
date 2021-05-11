40c587fdd4c610f10b90394213ba1206a17016b3
Kenneth Schnall <kenneth.schnall@icloud.com>
2021-05-10 18:09:38 -0700

    Update script, update README.md

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # reddit-account-migration
-
 ## How to use
 1. Clone the repository
-1. [Get a Reddit client ID and client secret](https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps)
-1. Paste your client ID and client secret in a new file named config.py in the root of the repository
+1. [Get a Reddit client ID and client secret for the account you're downloading from and the account you're uploading to](https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps)
+1. Paste your client IDs and client secrets in a new file named config.py in the root of the repository (DO NOT COMMIT THIS FILE)
 ```
-client_id = ''
-client_secret = ''
+DOWNLOAD_CLIENT_ID = ''
+DOWNLOAD_CLIENT_SECRET = ''
+
+UPLOAD_CLIENT_ID = ''
+UPLOAD_CLIENT_SECRET = ''
 ```
+1. Optionally put your account credentials in the config.py file (DO NOT COMMIT THIS FILE)
+```
+DOWNLOAD_PASSWORD = ''
+DOWNLOAD_USERNAME = ''
+
+UPLOAD_PASSWORD = ''
+UPLOAD_USERNAME = ''
+```
+1. Set up the venv: `py -m venv venv`
+1. Enter the venv: `.\venv\Scripts\Activate.ps1`
+1. Install the requirements: `pip install -r requirements.txt`
+1. Run the script: `py main.py -d -u`
--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import argparse
 import getpass
 import json
 import sys
+import time
 
 import praw
 
 import config
 
+# to do handle invalid password
+# to do handle invalid username
+
 DATA_DIRECTORY_NAME = 'data'
 FILE_OVERWRITE_MESSAGE_SUFFIX = ' already exists. Do you want to overwrite it?'
 FOUND_ACCOUNT_CREDENTIALS_MESSAGE = 'Found account credentials from config.py'
-GET_ACCOUNT_CREDENTIALS_MESSAGE_PREFIX = 'Enter account credentials for '
-REDDIT_OVERWRITE_MESSAGE = 'Do you want to upload this data to your Reddit account?'
+GET_ACCOUNT_CREDENTIALS_MESSAGE_PREFIX = '\nEnter account credentials for '
+REDDIT_OVERWRITE_MESSAGE = '\nDo you want to upload this data to your Reddit account?'
 USER_AGENT = 'reddit-account-migration'
 
 multireddits_filename = f'{DATA_DIRECTORY_NAME}/multireddits.json'
+skipped_resources_filename = f'{DATA_DIRECTORY_NAME}/skipped-resources.json'
 subreddits_filename = f'{DATA_DIRECTORY_NAME}/subreddits.json'
 
 
@@ -28,29 +33,16 @@ def confirm_exists(filename):
     """
     if not exists(filename):
         exit_script(f"Error: {filename} doesn't exist. Exiting.")
+ 
 
-
-def confirm_overwrite(message, reddit=None):
-    """Prompt the user to confirm if the resource should be overwritten and return the user's decision.
-    
-    Keyword arguments:
-    message -- the message to prompt the user with
-    reddit -- the PRAW Reddit instance (default None)
-    """
-    print(message)
-    if reddit:
-        print('Reddit account:', reddit.user.me().name)
-    user_input = input('(y/n)\n> ')
-    return user_input == 'y'
-
-
-def download_multireddits_from_reddit(reddit):
+def download_multireddits_from_reddit(reddit, should_prepend_newline=True):
     """Download multireddits from Reddit and return them.
     
     Keyword arguments:
     reddit -- the PRAW Reddit instance
+    should_prepend_newline -- whether or not to prepend the first print statement with a newline (default True)
     """
-    print('Downloading multireddits from Reddit')
+    print_message_prepend_newline('Downloading multireddits from Reddit', should_prepend_newline)
     multireddits = reddit.user.multireddits()
     multireddits_list = []
     for multireddit in multireddits:
@@ -72,11 +64,15 @@ def download_subreddits_from_reddit(reddit):
     Keyword arguments:
     reddit -- the PRAW Reddit instance
     """
-    print('Downloading subreddits from Reddit')
+    print('\nDownloading subreddits from Reddit')
     subreddits = reddit.user.subreddits(limit=None)
     subreddits_list = []
     for subreddit in subreddits:
-        subreddits_list.append(subreddit.display_name)
+        subreddits_list.append({
+            'displayName': subreddit.display_name,
+            'isQuarantined': subreddit.quarantine,
+            'subredditType': subreddit.subreddit_type,
+        })
         print('Subreddits downloaded:', len(subreddits_list))
     print('Total subreddits downloaded:', len(subreddits_list))
     return subreddits_list
@@ -126,7 +122,7 @@ def get_password():
     """Prompt the user to enter their Reddit account password and return it."""
     print('Password\n> ', end='')
     password = getpass.getpass(prompt='')
-    print('Confirm password\n>', end='')
+    print('Confirm password\n> ', end='')
     confirm_password = getpass.getpass(prompt='')
     return (confirm_password, password)
 
@@ -140,26 +136,51 @@ def get_subreddits_from_file():
         return subreddits
 
 
-def upload_multireddits_to_reddit(multireddits, reddit, should_overwrite):
+def print_message_prepend_newline(message, should_prepend_newline=True):
+    """Print a message prepended by a newline.
+    
+    Keyword arguments:
+    message -- the message to print
+    should_prepend_newline -- whether or not to prepend the message with a newline (default True)
+    """
+    message = f'\n{message}' if should_prepend_newline else message
+    print(message)
+
+
+def should_overwrite(message=REDDIT_OVERWRITE_MESSAGE, reddit=None):
+    """Return whether or not the resource should be overwritten.
+    
+    Keyword arguments:
+    message -- the message to prompt the user with (default REDDIT_OVERWRITE_MESSAGE)
+    reddit -- the PRAW Reddit instance (default None)
+    """
+    if not args.overwrite:
+        print(message)
+        if reddit:
+            print('Reddit account:', reddit.user.me().name)
+        user_input = input('(y/n)\n> ')
+        return user_input == 'y'
+    return True
+
+
+def upload_multireddits_to_reddit(multireddits, reddit):
     """Upload multireddits to Reddit, skipping any multireddit that already exists.
     
     Keyword arguments:
     multireddits -- the multireddits to upload to Reddit
     reddit -- the PRAW Reddit instance
-    should_overwrite -- whether or not the function should upload the multireddits to Reddit without confirming
     """
+    print('\nUploading multireddits to Reddit')
     print('Downloading existing multireddits to prevent collisions')
-    existing_multireddits = download_multireddits_from_reddit(reddit)
+    existing_multireddits = download_multireddits_from_reddit(reddit, False)
     existing_multireddits = [existing_multireddit['displayName'] for existing_multireddit in existing_multireddits]
-    print('Uploading multireddits to Reddit')
-    if not should_overwrite:
-        should_overwrite = confirm_overwrite(REDDIT_OVERWRITE_MESSAGE, reddit)
-        if not should_overwrite:
-            return
+    if not should_overwrite():
+        return
     multireddits_uploaded_count = 0
     for multireddit in multireddits:
         if multireddit['displayName'] in existing_multireddits:
-            print(f'Multireddit {multireddit["displayName"]} already exists, skipping')
+            print(f'Skipping multireddit {multireddit["displayName"]} as it already exists')
+            skipped_resources['multireddits'].append(multireddit)
             continue
         reddit.multireddit.create(multireddit['displayName'], multireddit['subreddits'], visibility=multireddit['visibility'])
         multireddits_uploaded_count += 1
@@ -167,59 +188,82 @@ def upload_multireddits_to_reddit(multireddits, reddit, should_overwrite):
     print('Total multireddits uploaded:', multireddits_uploaded_count)
 
 
-def upload_subreddits_to_reddit(reddit, should_overwrite, subreddits):
-    """Upload subreddits to Reddit.
+def upload_subreddits_to_reddit(reddit, subreddits):
+    """Upload subreddits to Reddit, skipping subreddits that are private or quarantined.
     
     Keyword arguments:
     reddit -- the PRAW Reddit instance
-    should_overwrite -- whether or not the function should upload the subreddits to Reddit without confirming
     subreddits -- the subreddits to upload to Reddit
     """
-    print('Uploading subreddits to Reddit')
-    if not should_overwrite:
-        should_overwrite = confirm_overwrite(REDDIT_OVERWRITE_MESSAGE, reddit)
-        if not should_overwrite:
-            return
-    subreddit_model_list = [reddit.subreddit(subreddit) for subreddit in subreddits[1:]]
-    # to do fix prawcore.exceptions.Forbidden: received 403 HTTP response when trying to subscribe
-    reddit.subreddit(subreddits[0]).subscribe(other_subreddits=subreddit_model_list)
-    print('Uploaded all subreddits')
+    print('\nUploading subreddits to Reddit')
+    if not should_overwrite():
+        return
+    subreddit_model_list = []
+    for subreddit in subreddits:
+        if subreddit['isQuarantined']:
+            print(f"Skipping subreddit {subreddit['displayName']} as it's quarantined")
+        elif subreddit['subredditType'] != 'private':
+            subreddit_model_list.append(reddit.subreddit(subreddit['displayName']))
+            continue
+        else:
+            print(f"Skipping subreddit {subreddit['displayName']} as it's private")
+        skipped_resources['subreddits'].append(subreddit)
+    batched_subreddit_model_list = []
+    subreddit_model_batch = []
+    for subreddit_model in subreddit_model_list:
+        subreddit_model_batch.append(subreddit_model)
+        if len(subreddit_model_batch) == 1000:
+            batched_subreddit_model_list.append(subreddit_model_batch)
+            subreddit_model_batch = []
+    if subreddit_model_batch:
+        batched_subreddit_model_list.append(subreddit_model_batch)
+    subreddits_uploaded_count = 0
+    for subreddit_model_batch in batched_subreddit_model_list:
+        reddit.subreddit(subreddit_model_batch[0].display_name).subscribe(other_subreddits=subreddit_model_batch[1:])
+        subreddits_uploaded_count += len(subreddit_model_batch)
+        print('Subreddits uploaded:', subreddits_uploaded_count)
+        time.sleep(1)
+    print('Total subreddits uploaded:', subreddits_uploaded_count)
 
 
-def write_multireddits_to_file(multireddits, should_overwrite):
+def write_multireddits_to_file(multireddits):
     """Write multireddits to file.
     
     Keyword arguments:
     multireddits -- the multireddits to save to the file
-    should_overwrite -- whether or not the function should write the multireddits to the file without confirming
     """
     dictionary = {
         'multireddits': multireddits
     }
     Path(DATA_DIRECTORY_NAME).mkdir(exist_ok=True, parents=True)
-    if not should_overwrite and exists(multireddits_filename):
-        should_overwrite = confirm_overwrite(f'{multireddits_filename}{FILE_OVERWRITE_MESSAGE_SUFFIX}')
-        if not should_overwrite:
-            return
+    if exists(multireddits_filename) and not should_overwrite(f'\n{multireddits_filename}{FILE_OVERWRITE_MESSAGE_SUFFIX}'):
+        return
     with open(multireddits_filename, 'w') as f:
         json.dump(dictionary, f)
 
 
-def write_subreddits_to_file(should_overwrite, subreddits):
+def write_skipped_resources_to_file():
+    """Write skipped resources to file."""
+    Path(DATA_DIRECTORY_NAME).mkdir(exist_ok=True, parents=True)
+    if exists(multireddits_filename) and not should_overwrite(f'\n{skipped_resources_filename}{FILE_OVERWRITE_MESSAGE_SUFFIX}'):
+        return
+    with open(skipped_resources_filename, 'w') as f:
+        json.dump(skipped_resources, f)
+    print(f'Skipped resources were written to {skipped_resources_filename}')
+
+
+def write_subreddits_to_file(subreddits):
     """Write subreddits to file.
     
     Keyword arguments:
-    should_overwrite -- whether or not the function should write the subreddits to the file without confirming
     subreddits -- the subreddits to save to the file
     """
     dictionary = {
         'subreddits': subreddits
     }
     Path(DATA_DIRECTORY_NAME).mkdir(exist_ok=True, parents=True)
-    if not should_overwrite and exists(subreddits_filename):
-        should_overwrite = confirm_overwrite(f'{subreddits_filename}{FILE_OVERWRITE_MESSAGE_SUFFIX}')
-        if not should_overwrite:
-            return
+    if exists(multireddits_filename) and not should_overwrite(f'\n{subreddits_filename}{FILE_OVERWRITE_MESSAGE_SUFFIX}'):
+        return
     with open(subreddits_filename, 'w') as f:
         json.dump(dictionary, f)
 
@@ -229,6 +273,11 @@ parser.add_argument('-d', '--download', action='store_true', help='Download data
 parser.add_argument('-u', '--upload', action='store_true', help='Upload data')
 parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite data (local data or Reddit data) without confirming')
 args = parser.parse_args()
+
+skipped_resources = {
+    'multireddits': [],
+    'subreddits': [],
+}
 
 if args.download:
     print('Downloading data from Reddit')
@@ -246,12 +295,12 @@ if args.download:
         username=account_credentials[1],
     )
     multireddits = download_multireddits_from_reddit(reddit)
-    write_multireddits_to_file(multireddits, args.overwrite)
+    write_multireddits_to_file(multireddits)
     subreddits = download_subreddits_from_reddit(reddit)
-    write_subreddits_to_file(args.overwrite, subreddits)
+    write_subreddits_to_file(subreddits)
 
 if args.upload:
-    print('Uploading data to Reddit')
+    print_message_prepend_newline('Uploading data to Reddit', args.download)
     account_credentials = None
     if hasattr(config, 'UPLOAD_PASSWORD') and hasattr(config, 'UPLOAD_USERNAME'):
         print(FOUND_ACCOUNT_CREDENTIALS_MESSAGE)
@@ -266,6 +315,7 @@ if args.upload:
         username=account_credentials[1],
     )
     multireddits = get_multireddits_from_file()
-    upload_multireddits_to_reddit(multireddits, reddit, args.overwrite)
+    upload_multireddits_to_reddit(multireddits, reddit)
     subreddits = get_subreddits_from_file()
-    upload_subreddits_to_reddit(reddit, args.overwrite, subreddits)
+    upload_subreddits_to_reddit(reddit, subreddits)
+    write_skipped_resources_to_file()
